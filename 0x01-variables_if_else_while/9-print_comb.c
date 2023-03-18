acad410cfef2001c4bfa21f7aabc03bfba944733
mazen95 <mazen7m7mohamed@gmail.com>
2023-03-18 14:46:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+/**
+ * main - prints all possible combinations,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char n;
+char i;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar(',');
+putchar(' ');
+}
