626869aada74b3a3cef5782c343c70906c822803
mazen95 <mazen7m7mohamed@gmail.com>
2023-03-19 12:24:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  * Return: 0
  */
 int main(void)
-	{
-		int x;
-		int y;
+{
+	int c;
+	int d = 0;
 
-	for (x = 48; x < 57; x++)
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
 		{
-	for (y = 48; y < 57; y++)
+			if (d != c && d < c)
 			{
-			if (y >=  x)
-			{
-				putchar(x);
-				putchar(y);
-				putchar(',');
-				putchar(' ');
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			}
+
+			c++;
 		}
-	return (0);
+		d++;
 	}
+	putchar('\n');
+	return (0);
+}
