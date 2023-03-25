c004899a6e1a732a505c38b4716bb19679144deb
mazen95 <mazen7m7mohamed@gmail.com>
2023-03-24 19:55:39 -0700

    3m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,20 @@
-/*
- * File: 3-print_numbers.c
- * Auth: Ephrem Abebe
- */
-
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_most_numbers - prints the numbers from 0 to 9
+ * except 2 and 4
+ * Return: no return
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
+	for  (num = '0'; num <= '9'; num++)
+	{
+		if (num != '2' && num != '4')
+		{
+			_putchar(num);
+		}
+	}
 	_putchar('\n');
 }
