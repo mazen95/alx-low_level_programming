5d3b9acb93f06848ed58e94a08ef43fe29861dd5
mazen95 <mazen7m7mohamed@gmail.com>
2023-03-25 14:37:42 -0700

    fin com

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,3 +13,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+#endif
