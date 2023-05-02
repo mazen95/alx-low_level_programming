20eb8bfa42a87fa75478b86032621052a0b5c44e
mazen95 <mazen7m7mohamed@gmail.com>
2023-05-02 13:06:01 -0700

    wfsda

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,27 +1,45 @@
-include "lists.h"
+/*
+ * File: 2-add_node.c
+ * Auth: RG Mashaal
+ */
+
+#include "lists.h"
+#include <string.h>
 
 /**
- * Authr: RG Mashaal
- * add_nodeint - adds a node to the beginning of a linked list
- * @head: pointer to the head of the list
- * @n: integer to be used as content
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
  *
- * Return: address of the newly added node
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+list_t *add_node(list_t **head, const char *str)
 {
-	listint_t *new_node;
+	char *dup;
+	int len;
+	list_t *new;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = *head;
-	}
-	else
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	if (*head != NULL)
-		new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
