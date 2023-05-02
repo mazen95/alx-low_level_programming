1d7e3c3a8fc949dd436c518b1329e7badeea27b2
mazen95 <mazen7m7mohamed@gmail.com>
2023-05-02 16:50:29 -0700

    lkjfsa

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,6 @@
 
 /**
  * insert_nodeint_at_index - Inserts a new node to a listint_t
- *                           list at a given position.
  * @head: A pointer to the address of the
  *        head of the listint_t list.
  * @idx: The index of the listint_t list where the new
